da985db59599c23a7a675ad552b91f7d8a444763
yaasien-s <66425757+yaasien-s@users.noreply.github.com>
2021-12-01 14:09:04 +0200

    commit

---
+++ script.js
@@ -0,0 +1,70 @@
+let idealBmi = document.getElementById('IdealBmi');
+let comment = document.getElementById('comment');
+
+function myBMI(){
+    let w = document.getElementById("weight").value;
+    let h = document.getElementById("height").value;
+    let ans = w/(h**2);
+    document.getElementById("outputBmi").innerHTML = `Your BMI is ${ans}`
+    
+    let age = document.getElementById('age')
+
+    if(document.getElementById('gndr').value == 'male'){
+        let ansMale = 0.5*ans + 11.5;
+        idealBmi.innerHTML = `Ideal BMI is ${ansMale}`
+    }else if(document.getElementById('gndr').value == 'female'){
+        let ansMale = 0.5*ans + 0.03*age.value + 11;
+        idealBmi.innerHTML = `Ideal BMI is ${ansMale}`
+    }
+
+
+    if(ans < 18){
+        comment.innerText = "Underweight"
+    }else if(ans >= 18 && ans < 25){
+        comment.innerText = "Healthy"
+    }else if(ans >= 25 && ans < 30){
+        comment.innerText = "Overweight"
+    }else{
+        comment.innerText = "Obese"
+    }
+    
+}
+
+function disableInput(){
+    if(document.getElementById('gndr').value == 'male'){
+        document.getElementById('age').disabled = true;
+    }else{
+        document.getElementById('age').disabled = false;
+    }
+    
+}
+
+
+function clear_inputs(){
+    let inputs = document.getElementsByClassName('input')
+
+    for(x of inputs){
+        x.value = '';
+    }
+
+    document.getElementById('gndr').value = 'empty';
+    document.getElementById("outputBmi").innerHTML = "";
+    idealBmi.innerText = "";
+    comment.innerText = "";
+}
+
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
